31be2c2705cc973881f8fa0427e67e1891d97489
Camila <camila.utz@hotmail.com>
2022-07-22 11:34:52 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc_/Data/SalesWebMvc_Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMCV.Models;
+
+namespace SalesWebMvc_.Data
+{
+    public class SalesWebMvc_Context : DbContext
+    {
+        public SalesWebMvc_Context (DbContextOptions<SalesWebMvc_Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMCV.Models.Departament> Departament { get; set; }
+    }
+}
---
+++ SalesWebMvc_/Models/Departament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMCV.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
